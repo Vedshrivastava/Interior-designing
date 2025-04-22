a619bccd8b6082bf390f01fd8aca83fee9bd641b
Ved Shrivastava <vedshrivastava1357@gmail.com>
2025-04-22 23:20:04 +0530

    some more changes

--- Frontend/src/pages/designDisplay.jsx
+++ Frontend/src/pages/designDisplay.jsx
@@ -58,12 +58,13 @@ const DesignDisplay = ({ setShowLogin, setConsultData, consultData }) => {
     draggable: true,
   };
 
+
   return (
     <div>
       <div className='design-display-main' id='design-display'>
 
         {/* New Design Navbar */}
-        <MainNavbar setShowLogin={setShowLogin}/>
+        <MainNavbar setShowLogin={setShowLogin} />
 
         <h2>{currentCategory}</h2>
         <p className='main-para'>
@@ -72,105 +73,105 @@ const DesignDisplay = ({ setShowLogin, setConsultData, consultData }) => {
 
         {/* Conditional Rendering for Slider or Select */}
         <div className='select-window'>
-        {isMobileView ? (
-  <div className="category-slider">
-    <div className="slider-container">
-      <div className="slider-item" onClick={() => handleSliderClick('Kitchen Designs')}>
-        <p className={currentCategory === 'Kitchen Designs' ? 'selected-category' : ''}>Kitchen Designs</p>
-      </div>
-      <div className="slider-item" onClick={() => handleSliderClick('Bedroom Designs')}>
-        <p className={currentCategory === 'Bedroom Designs' ? 'selected-category' : ''}>Bedroom Designs</p>
-      </div>
-      <div className="slider-item" onClick={() => handleSliderClick('Bathroom Designs')}>
-        <p className={currentCategory === 'Bathroom Designs' ? 'selected-category' : ''}>Bathroom Designs</p>
-      </div>
-      <div className="slider-item" onClick={() => handleSliderClick('Lounge area Designs')}>
-        <p className={currentCategory === 'Lounge area Designs' ? 'selected-category' : ''}>Lounge area</p>
-      </div>
-      <div className="slider-item" onClick={() => handleSliderClick('Home Cinema Designs')}>
-        <p className={currentCategory === 'Home Cinema Designs' ? 'selected-category' : ''}>Home Cinema</p>
-      </div>
-      <div className="slider-item" onClick={() => handleSliderClick('TV Unit Designs')}>
-        <p className={currentCategory === 'TV Unit Designs' ? 'selected-category' : ''}>TV Unit</p>
-      </div>
-      <div className="slider-item" onClick={() => handleSliderClick('Commercial Designs')}>
-        <p className={currentCategory === 'Commercial Designs' ? 'selected-category' : ''}>Commercial Designs</p>
-      </div>
-      <div className="slider-item" onClick={() => handleSliderClick('House Exterior')}>
-        <p className={currentCategory === 'House Exterior' ? 'selected-category' : ''}>House Exterior</p>
-      </div>
-      <div className="slider-item" onClick={() => handleSliderClick('PVC Louvers')}>
-        <p className={currentCategory === 'PVC Louvers' ? 'selected-category' : ''}>PVC Louvers</p>
-      </div>
-      <div className="slider-item" onClick={() => handleSliderClick('WPC Louvers')}>
-        <p className={currentCategory === 'WPC Louvers' ? 'selected-category' : ''}>WPC Louvers</p>
-      </div>
-      <div className="slider-item" onClick={() => handleSliderClick('Charcoal Louvers')}>
-        <p className={currentCategory === 'Charcoal Louvers' ? 'selected-category' : ''}>Charcoal Louvers</p>
-      </div>
-      <div className="slider-item" onClick={() => handleSliderClick('Five G Louvers')}>
-        <p className={currentCategory === 'Five G Louvers' ? 'selected-category' : ''}>Five G Louvers</p>
-      </div>
-      <div className="slider-item" onClick={() => handleSliderClick('Marble sheets')}>
-        <p className={currentCategory === 'Marble sheets' ? 'selected-category' : ''}>Marble sheets</p>
-      </div>
-      <div className="slider-item" onClick={() => handleSliderClick('Acrylic Sheets')}>
-        <p className={currentCategory === 'Acrylic Sheets' ? 'selected-category' : ''}>Acrylic Sheets</p>
-      </div>
-      <div className="slider-item" onClick={() => handleSliderClick('Flooring')}>
-        <p className={currentCategory === 'Flooring' ? 'selected-category' : ''}>Flooring</p>
-      </div>
-    </div>
-  </div>
-) : (
-          <Slider {...sliderSettings} className="design-slider-bar">
-            <div onClick={() => handleSliderClick('Kitchen Designs')}>
-              <p className={currentCategory === 'Kitchen Designs' ? 'selected-category' : ''}>Kitchen Designs</p>
+          {isMobileView ? (
+            <div className="category-slider">
+              <div className="slider-container">
+                <div className="slider-item" onClick={() => handleSliderClick('Kitchen Designs')}>
+                  <p className={currentCategory === 'Kitchen Designs' ? 'selected-category' : ''}>Kitchen Designs</p>
+                </div>
+                <div className="slider-item" onClick={() => handleSliderClick('Bedroom Designs')}>
+                  <p className={currentCategory === 'Bedroom Designs' ? 'selected-category' : ''}>Bedroom Designs</p>
+                </div>
+                <div className="slider-item" onClick={() => handleSliderClick('Bathroom Designs')}>
+                  <p className={currentCategory === 'Bathroom Designs' ? 'selected-category' : ''}>Bathroom Designs</p>
+                </div>
+                <div className="slider-item" onClick={() => handleSliderClick('Lounge area Designs')}>
+                  <p className={currentCategory === 'Lounge area Designs' ? 'selected-category' : ''}>Lounge area</p>
+                </div>
+                <div className="slider-item" onClick={() => handleSliderClick('Home Cinema Designs')}>
+                  <p className={currentCategory === 'Home Cinema Designs' ? 'selected-category' : ''}>Home Cinema</p>
+                </div>
+                <div className="slider-item" onClick={() => handleSliderClick('TV Unit Designs')}>
+                  <p className={currentCategory === 'TV Unit Designs' ? 'selected-category' : ''}>TV Unit</p>
+                </div>
+                <div className="slider-item" onClick={() => handleSliderClick('Commercial Designs')}>
+                  <p className={currentCategory === 'Commercial Designs' ? 'selected-category' : ''}>Commercial Designs</p>
+                </div>
+                <div className="slider-item" onClick={() => handleSliderClick('House Exterior')}>
+                  <p className={currentCategory === 'House Exterior' ? 'selected-category' : ''}>House Exterior</p>
+                </div>
+                <div className="slider-item" onClick={() => handleSliderClick('PVC Louvers')}>
+                  <p className={currentCategory === 'PVC Louvers' ? 'selected-category' : ''}>PVC Louvers</p>
+                </div>
+                <div className="slider-item" onClick={() => handleSliderClick('WPC Louvers')}>
+                  <p className={currentCategory === 'WPC Louvers' ? 'selected-category' : ''}>WPC Louvers</p>
+                </div>
+                <div className="slider-item" onClick={() => handleSliderClick('Charcoal Louvers')}>
+                  <p className={currentCategory === 'Charcoal Louvers' ? 'selected-category' : ''}>Charcoal Louvers</p>
+                </div>
+                <div className="slider-item" onClick={() => handleSliderClick('Five G Louvers')}>
+                  <p className={currentCategory === 'Five G Louvers' ? 'selected-category' : ''}>Five G Louvers</p>
+                </div>
+                <div className="slider-item" onClick={() => handleSliderClick('Marble sheets')}>
+                  <p className={currentCategory === 'Marble sheets' ? 'selected-category' : ''}>Marble sheets</p>
+                </div>
+                <div className="slider-item" onClick={() => handleSliderClick('Acrylic Sheets')}>
+                  <p className={currentCategory === 'Acrylic Sheets' ? 'selected-category' : ''}>Acrylic Sheets</p>
+                </div>
+                <div className="slider-item" onClick={() => handleSliderClick('Flooring')}>
+                  <p className={currentCategory === 'Flooring' ? 'selected-category' : ''}>Flooring</p>
+                </div>
+              </div>
             </div>
-            <div onClick={() => handleSliderClick('Bedroom Designs')}>
-              <p className={currentCategory === 'Bedroom Designs' ? 'selected-category' : ''}>Bedroom Designs</p>
-            </div>
-            <div onClick={() => handleSliderClick('Bathroom Designs')}>
-              <p className={currentCategory === 'Bathroom Designs' ? 'selected-category' : ''}>Bathroom Designs</p>
-            </div>
-            <div onClick={() => handleSliderClick('Lounge area Designs')}>
-              <p className={currentCategory === 'Lounge area Designs' ? 'selected-category' : ''}>Lounge area</p>
-            </div>
-            <div onClick={() => handleSliderClick('Home Cinema Designs')}>
-              <p className={currentCategory === 'Home Cinema Designs' ? 'selected-category' : ''}>Home Cinema</p>
-            </div>
-            <div onClick={() => handleSliderClick('TV Unit Designs')}>
-              <p className={currentCategory === 'Kids Room Designs' ? 'selected-category' : ''}>TV Unit</p>
-            </div>
-            <div onClick={() => handleSliderClick('Commercial Designs')}>
-              <p className={currentCategory === 'Commercial Designs' ? 'selected-category' : ''}>Commercial Designs</p>
-            </div>
-            <div onClick={() => handleSliderClick('House Exterior')}>
-              <p className={currentCategory === 'House Exterior' ? 'selected-category' : ''}>House Exterior</p>
-            </div>
-            <div onClick={() => handleSliderClick('PVC Louvers')}>
-              <p className={currentCategory === 'PVC Louvers' ? 'selected-category' : ''}>PVC Louvers</p>
-            </div>
-            <div onClick={() => handleSliderClick('WPC Louvers')}>
-              <p className={currentCategory === 'WPC Louvers' ? 'selected-category' : ''}>WPC Louvers</p>
-            </div>
-            <div onClick={() => handleSliderClick('Charcoal Louvers')}>
-              <p className={currentCategory === 'Charcoal Louvers' ? 'selected-category' : ''}>Charcoal Louvers</p>
-            </div>
-            <div onClick={() => handleSliderClick('Five G Louvers')}>
-              <p className={currentCategory === 'Five G Louvers' ? 'selected-category' : ''}>Five G Louvers</p>
-            </div>
-            <div onClick={() => handleSliderClick('Marble sheets')}>
-              <p className={currentCategory === 'Marble sheets' ? 'selected-category' : ''}>Marble sheets</p>
-            </div>
-            <div onClick={() => handleSliderClick('Acrylic Sheets')}>
-              <p className={currentCategory === 'Acrylic Sheets' ? 'selected-category' : ''}>Acrylic Sheets</p>
-            </div>
-            <div onClick={() => handleSliderClick('Flooring')}>
-              <p className={currentCategory === 'Flooring' ? 'selected-category' : ''}>Flooring</p>
-            </div>
-          </Slider>
-        )}
+          ) : (
+            <Slider {...sliderSettings} className="design-slider-bar">
+              <div onClick={() => handleSliderClick('Kitchen Designs')}>
+                <p className={currentCategory === 'Kitchen Designs' ? 'selected-category' : ''}>Kitchen Designs</p>
+              </div>
+              <div onClick={() => handleSliderClick('Bedroom Designs')}>
+                <p className={currentCategory === 'Bedroom Designs' ? 'selected-category' : ''}>Bedroom Designs</p>
+              </div>
+              <div onClick={() => handleSliderClick('Bathroom Designs')}>
+                <p className={currentCategory === 'Bathroom Designs' ? 'selected-category' : ''}>Bathroom Designs</p>
+              </div>
+              <div onClick={() => handleSliderClick('Lounge area Designs')}>
+                <p className={currentCategory === 'Lounge area Designs' ? 'selected-category' : ''}>Lounge area</p>
+              </div>
+              <div onClick={() => handleSliderClick('Home Cinema Designs')}>
+                <p className={currentCategory === 'Home Cinema Designs' ? 'selected-category' : ''}>Home Cinema</p>
+              </div>
+              <div onClick={() => handleSliderClick('TV Unit Designs')}>
+                <p className={currentCategory === 'Kids Room Designs' ? 'selected-category' : ''}>TV Unit</p>
+              </div>
+              <div onClick={() => handleSliderClick('Commercial Designs')}>
+                <p className={currentCategory === 'Commercial Designs' ? 'selected-category' : ''}>Commercial Designs</p>
+              </div>
+              <div onClick={() => handleSliderClick('House Exterior')}>
+                <p className={currentCategory === 'House Exterior' ? 'selected-category' : ''}>House Exterior</p>
+              </div>
+              <div onClick={() => handleSliderClick('PVC Louvers')}>
+                <p className={currentCategory === 'PVC Louvers' ? 'selected-category' : ''}>PVC Louvers</p>
+              </div>
+              <div onClick={() => handleSliderClick('WPC Louvers')}>
+                <p className={currentCategory === 'WPC Louvers' ? 'selected-category' : ''}>WPC Louvers</p>
+              </div>
+              <div onClick={() => handleSliderClick('Charcoal Louvers')}>
+                <p className={currentCategory === 'Charcoal Louvers' ? 'selected-category' : ''}>Charcoal Louvers</p>
+              </div>
+              <div onClick={() => handleSliderClick('Five G Louvers')}>
+                <p className={currentCategory === 'Five G Louvers' ? 'selected-category' : ''}>Five G Louvers</p>
+              </div>
+              <div onClick={() => handleSliderClick('Marble sheets')}>
+                <p className={currentCategory === 'Marble sheets' ? 'selected-category' : ''}>Marble sheets</p>
+              </div>
+              <div onClick={() => handleSliderClick('Acrylic Sheets')}>
+                <p className={currentCategory === 'Acrylic Sheets' ? 'selected-category' : ''}>Acrylic Sheets</p>
+              </div>
+              <div onClick={() => handleSliderClick('Flooring')}>
+                <p className={currentCategory === 'Flooring' ? 'selected-category' : ''}>Flooring</p>
+              </div>
+            </Slider>
+          )}
         </div>
 
         <div className="design-display-list">

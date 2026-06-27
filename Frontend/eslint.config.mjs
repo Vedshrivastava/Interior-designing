e2e507bfda5397f400aad264e6131f38a662a0ca
Ved Shrivastava <vedshrivastava1357@gmail.com>
2026-06-27 13:25:45 +0530

    disable strict eslint rules for build

--- Frontend/eslint.config.mjs
+++ Frontend/eslint.config.mjs
@@ -3,14 +3,18 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
-  // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "react-hooks/set-state-in-effect": "off",
+      "@next/next/no-img-element": "off",
+    },
+  },
 ]);
 
 export default eslintConfig;

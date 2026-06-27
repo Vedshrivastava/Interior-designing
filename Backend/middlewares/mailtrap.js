f81283245d42dc8b90a3e19858f42b1fb71ce9b5
Ved Shrivastava <vedshrivastava1357@gmail.com>
2026-06-28 01:50:58 +0530

    add secure: false for port 587 STARTTLS

--- Backend/middlewares/mailtrap.js
+++ Backend/middlewares/mailtrap.js
@@ -6,6 +6,7 @@ dotenv.config();
 export const transporter = nodemailer.createTransport({
   host: process.env.MAILTRAP_HOST || 'sandbox.smtp.mailtrap.io',
   port: parseInt(process.env.MAILTRAP_PORT || '2525'),
+  secure: false,
   auth: {
     user: process.env.MAILTRAP_USER,
     pass: process.env.MAILTRAP_PASS,
